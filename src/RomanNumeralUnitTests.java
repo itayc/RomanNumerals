e7505f9088ba5baacbb7864ecb9f4d2a734dff28
Itay Cohen <itay@yourdomain.com>
2012-12-16 13:06:29 +0200

    RED

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -12,7 +12,7 @@ public class RomanNumeralUnitTests {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
 		assertEquals("M", number.toRoman(1000));
-		assertEquals("XIX", number.toRoman(19));
+		assertEquals("XIX", number.toRoman(3));
 		assertEquals("XLIX", number.toRoman(49));
 		//assertEquals("II", number.toRoman(2));
 	}
